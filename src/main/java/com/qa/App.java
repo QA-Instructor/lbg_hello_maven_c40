b647c07f09cb8be510d932e5744547b77982fea3
jordanbenbelaid <jordanbenbelaid@hotmail.com>
2026-04-21 16:08:15 +0100

    EDIT: version 1.5 release build, hello everyone

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,6 +12,8 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Everyone"));
+        System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
